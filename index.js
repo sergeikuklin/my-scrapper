2c046aca90ac9d9f13e52eae70ad1d25a448ba06
Sergei Kuklin <sergei.kuklin@forto.com>
2025-03-16 01:49:10 +0100

    bump

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-import { checkBurgerTestTerminJob } from './checkBurgerTestTermin.js';
-import { sendMessage } from './sendMessage.js';
-
-// checkBurgerTestTerminJob.start();
 console.log('test');
-
-sendMessage('test');
