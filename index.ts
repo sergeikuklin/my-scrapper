d989cc1155ca681244bdddbdc49361f9fed91bbb
Sergei Kuklin <sergei.kuklin@forto.com>
2025-03-16 01:39:02 +0100

    Test

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 import { checkBurgerTestTerminJob } from './checkBurgerTestTermin.ts';
+import { sendMessage } from './sendMessage.ts';
 
-checkBurgerTestTerminJob.start();
+// checkBurgerTestTerminJob.start();
+console.log('test');
+
+sendMessage('test');
